fc13f86eb0dcada614eab1eff65273da9e5cd519
BuildTools <unconfigured@null.spigotmc.org>
2018-07-20 15:47:11 +0200

    Revert "Update bmdetectnew.js"
    This reverts commit 1a7a30de0e41192c0222c4ffc6511865a3922ddc.

--- src/monitors/bmdetectnew.js
+++ src/monitors/bmdetectnew.js
@@ -19,63 +19,41 @@ module.exports = class extends Monitor {
     }
 
     async run(msg) {
-
-        
+        var idmessage = msg.content
+        var color = randomColor(); // a hex code for an attractive color
+        var IDbeatmap = idmessage.split('/').pop()
+        //console.log(IDbeatmap)
         if(msg.content.startsWith('https://osu.ppy.sh/beatmapsets/')){
-
-        msg.channel.send('Processing !').then( sent => {
-            var execOJSAMA = require('child_process').exec;
-
-            var idmessage = msg.content
-            var color = randomColor(); // a hex code for an attractive color
-            var IDbeatmap = idmessage.split('/').pop()
-            // console.log(IDbeatmap)
-
-            // console.log('/b/')
-        execOJSAMA('curl https://osu.ppy.sh/osu/' + IDbeatmap + ' | node ojsama.js',  function (error, stdout, stderr) {
-            // console.log('stdout: ' + stdout);
-            // message.reply(stdout)
-            var out = stdout.split('\n')
-            // console.log('stdout split : ' + out)
-            // console.log(Array.from(out)) 
-            // console.log('stderr: ' + stderr);
-            if (error !== null) {
-              console.log('exec error: ' + error);
-            }
+            // console.log('beatmapsets')
             //Api call to get map data
-            api.beatmaps
-            .getByBeatmapId(IDbeatmap)
-            .then(beatmaps=> {
-                //console.log(beatmaps[0])
-  
+        api.beatmaps
+        .getByBeatmapId(IDbeatmap)
+        .then(beatmaps=> {
+            // console.log(beatmaps[0])
+        
+        // If Else statements are mainly made for fun ( viva private joke )
+
+        // Creating Embed 
+        var embed = new MessageEmbed()
+        .setTitle(beatmaps[0]['artist'] + ' - ' + beatmaps[0]['title'] + '[' + beatmaps[0]['version'] + ']', ' (map by' + beatmaps[0]['creator'] + ')'  )
+        .addField('❯ Star Rating', beatmaps[0]['difficultyrating'].slice(0,4) ,true)
+        .addField('❯ Stats', 'AR' + beatmaps[0]['diff_approach'] + ' OD' + beatmaps[0]['diff_overall'] + ' CS' + beatmaps[0]['diff_size'] + ' HP' + beatmaps[0]['diff_drain'], true)
+        .addField('❯ Max Combo', beatmaps[0]['max_combo']+ 'x', true)
+        .addField('❯ BPM', beatmaps[0]['bpm'], true)
+        .addField('❯ Length', Math.floor(beatmaps[0]['total_length']/60) + 'm' + Math.floor(beatmaps[0]['total_length']%60) +'s' + ' (*' + Math.floor(beatmaps[0]['hit_length']/60) + 'm' + Math.floor(beatmaps[0]['hit_length']%60) +'s' + (' drain*)'),true)
     
-            var PPtoSplit = out[10]
-            var pp = PPtoSplit.split(/\s+/)
-            // Creating Embed 
-            var embed = new MessageEmbed()
-            .setTitle(beatmaps[0]['artist'] + ' - ' + beatmaps[0]['title'] + '[' + beatmaps[0]['version'] + ']', ' (map by' + beatmaps[0]['creator'] + ')'  )
-            .addField('❯ Star Rating', beatmaps[0]['difficultyrating'].slice(0,4) ,true)
-            .addField('❯ Stats', 'AR' + beatmaps[0]['diff_approach'] + ' OD' + beatmaps[0]['diff_overall'] + ' CS' + beatmaps[0]['diff_size'] + ' HP' + beatmaps[0]['diff_drain'], true)
-            .addField('❯ Max Combo', beatmaps[0]['max_combo']+ 'x', true)
-            .addField('❯ BPM', beatmaps[0]['bpm'], true)
-            .addField('❯ Length', Math.floor(beatmaps[0]['total_length']/60) + 'm' + Math.floor(beatmaps[0]['total_length']%60) +'s' + ' (*' + Math.floor(beatmaps[0]['hit_length']/60) + 'm' + Math.floor(beatmaps[0]['hit_length']%60) +'s' + (' drain*)'),true)
-        
-            .setFooter('100%: ' + pp[0] + ' ' + pp [1])
-            .setURL('https://osu.ppy.sh/b/'+ beatmaps[0].beatmap_id)
-            .setColor(color) 
-            .setThumbnail('https://b.ppy.sh/thumb/' + beatmaps[0].beatmapset_id + 'l.jpg')
-           
-            //post embed to channel
-            sent.edit({embed});
-        })
-        
-        })
-            
-            
-         });
-
-
-
+        .setFooter('owo')
+        .setURL('https://osu.ppy.sh/b/'+ beatmaps[0].beatmap_id)
+        .setColor(color) 
+        .setThumbnail('https://b.ppy.sh/thumb/' + beatmaps[0].beatmapset_id + 'l.jpg')
+       
+        //post embed to channel
+        msg.channel.send({embed});
+    
+    
+    })
+        }
 }
+
     }
-    }
+
